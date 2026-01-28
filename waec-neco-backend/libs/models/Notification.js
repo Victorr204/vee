5b449c6a38f3588635324d98c1c8ec8d969e2572
victor reuben <130871216+Victorr204@users.noreply.github.com>
2026-01-28 03:04:44 +0100

    exam sharp school update

--- waec-neco-backend/libs/models/Notification.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const NotificationSchema = new mongoose.Schema({
-  text: String,
-  expiresAt: Date,
-});
-
-export default mongoose.models.Notification ||
-  mongoose.model("Notification", NotificationSchema);
