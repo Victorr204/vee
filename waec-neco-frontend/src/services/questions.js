1a9f2bc22561d2779a1d6837eadd5067950f5179
victor reuben <130871216+Victorr204@users.noreply.github.com>
2026-01-28 04:38:00 +0100

    exam sharp school update

--- waec-neco-frontend/src/services/questions.js
+++ waec-neco-frontend/src/services/questions.js
@@ -3,11 +3,13 @@
 export async function fetchPublicQuestions() {
   try {
     const res = await fetch("https://waec-neco-backend.vercel.app/api/past-questions");
+
     if (!res.ok) throw new Error("Failed to fetch questions");
-    const data = await res.json();
-    return data;
+
+    return await res.json();
   } catch (err) {
     console.error("fetchPublicQuestions error:", err);
     return [];
   }
 }
+
